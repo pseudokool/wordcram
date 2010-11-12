4c7a434ebd3092bcf2a54632f20dca2987676df4
danbernier <danbernier@db2b9112-8060-c212-668a-ea1c16feaf2a>
2010-11-12 17:23:06 +0000

    making a dev branch for testing out PDF rendering

---
+++ branches/DEV_PDF/example/helloworld/helloworld.pde
@@ -0,0 +1,41 @@
+/*
+Copyright 2010 Daniel Bernier
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+This is as basic as it gets.  If you can't get this running, 
+something is not quite right.
+*/
+
+import wordcram.*;
+
+size(700, 400);
+background(0);
+
+// Each Word object has its word, and its weight.  You can use whatever
+// numbers you like for their weights, and they can be in any order.
+Word[] wordArray = new Word[] {
+    new Word("Hello", 100),
+    new Word("WordCram", 60)
+  };
+
+// Pass in the sketch (the variable "this"), so WordCram can draw to it.
+WordCram wordcram = new WordCram(this)
+
+// Pass in the words to draw.
+  .fromWords(wordArray);
+
+// Now we've created our WordCram, we can draw it:
+wordcram.drawAll();
---
+++ branches/DEV_PDF/src/wordcram/Word.java
@@ -0,0 +1,73 @@
+package wordcram;
+
+import processing.core.PVector;
+
+/*
+Copyright 2010 Daniel Bernier
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+public class Word implements Comparable<Word> {
+	public String word;
+	public double weight;
+	
+	private PVector desiredLocation;
+	private PVector currentLocation;
+	private BBTree bbTree;
+		
+	public Word(String word, double weight) {
+		this.word = word;
+		this.weight = weight;
+	}
+	
+	@Override
+	public String toString() {
+		return word + " (" + weight + ")";
+	}
+
+	@Override
+	public int compareTo(Word w) {
+		if (w.weight < weight) {
+			return -1;
+		}
+		else if (w.weight > weight) {
+			return 1;
+		}
+		else return 0;
+	}
+	
+	public void setBBTree(BBTree _bbTree) {
+		bbTree = _bbTree;
+	}
+
+	public boolean overlaps(Word other) {
+		return bbTree.overlaps(other.bbTree);
+	}
+	
+	public void setDesiredLocation(PVector loc) {
+		desiredLocation = new PVector(loc.x, loc.y);
+		currentLocation = new PVector(loc.x, loc.y);
+	}
+	public void nudge(PVector nudge) {
+		currentLocation = PVector.add(desiredLocation, nudge);
+		bbTree.setLocation(currentLocation.get());
+	}
+	public PVector getLocation() {
+		return currentLocation.get();
+	}
+	
+	BBTree getBBTree() {
+		return bbTree;
+	}
+}
