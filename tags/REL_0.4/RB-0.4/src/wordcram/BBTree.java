6bf9a34b51861f778f77c25567d2ef63d21c1ca4
danbernier <danbernier@db2b9112-8060-c212-668a-ea1c16feaf2a>
2011-03-17 22:12:53 +0000

    Added release 0.4

---
+++ tags/REL_0.4/RB-0.4/src/wordcram/BBTree.java
@@ -0,0 +1,143 @@
+package wordcram;
+
+import java.util.ArrayList;
+
+import processing.core.*;
+
+/*
+ Copyright 2010 Daniel Bernier
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+class BBTree {
+	private int left;
+	private int top;
+	private int right;
+	private int bottom;
+	private BBTree[] kids;
+
+	private PVector location = new PVector(0, 0);
+
+	BBTree(int _left, int _top, int _right, int _bottom) {
+		left = _left;
+		top = _top;
+		right = _right;
+		bottom = _bottom;
+	}
+
+	void addKids(BBTree... _kids) {
+		ArrayList<BBTree> kidList = new ArrayList<BBTree>();
+		for (BBTree kid : _kids) {
+			if (kid != null) {
+				kidList.add(kid);
+			}
+		}
+
+		kids = kidList.toArray(new BBTree[0]);
+	}
+
+	void setLocation(PVector _location) {
+		location = _location;
+		if (!isLeaf()) {
+			for (BBTree kid : kids) {
+				kid.setLocation(_location);
+			}
+		}
+	}
+
+	boolean overlaps(BBTree otherTree) {
+
+		if (rectCollide(this, otherTree)) {
+			if (this.isLeaf() && otherTree.isLeaf()) {
+				return true;
+			}
+
+			if (this.isLeaf()) {  // Then otherTree isn't a leaf.
+				for (BBTree otherKid : otherTree.kids) {
+					if (this.overlaps(otherKid)) {
+						return true;
+					}
+				}
+				return false; // This isLeaf, but doesn't overlap w/ any otherTree's kids.
+			}
+			
+			// Now we know that neither this nor otherTree are leaves.
+			for (BBTree myKid : this.kids) {
+				if (otherTree.overlaps(myKid)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private PVector[] getPoints() {
+		return new PVector[] { PVector.add(new PVector(left, top), location),
+				PVector.add(new PVector(right, bottom), location) };
+	}
+
+	private boolean rectCollide(BBTree a, BBTree b) {
+		PVector[] aPoints = a.getPoints();
+		PVector[] bPoints = b.getPoints();
+		PVector aTopLeft = aPoints[0];
+		PVector aBottomRight = aPoints[1];
+		PVector bTopLeft = bPoints[0];
+		PVector bBottomRight = bPoints[1];
+
+		return aBottomRight.y > bTopLeft.y && aTopLeft.y < bBottomRight.y
+				&& aBottomRight.x > bTopLeft.x && aTopLeft.x < bBottomRight.x;
+	}
+
+	boolean isLeaf() {
+		return kids == null;
+	}
+
+	void swellLeaves(int extra) {
+		if (isLeaf()) {
+			left -= extra;
+			right += extra;
+			top -= extra;
+			bottom += extra;
+		} else {
+			for (int i = 0; i < kids.length; i++) {
+				kids[i].swellLeaves(extra);
+			}
+		}
+	}
+
+	void draw(PGraphics g) {
+		g.pushStyle();
+		g.rectMode(PConstants.CORNERS);
+		g.noFill();
+	
+		g.stroke(30, 255, 255, 50);
+		drawLeaves(g);
+			
+		g.popStyle();
+	}
+
+	private void drawLeaves(PGraphics g) {
+		if (this.isLeaf()) {
+			drawBounds(g, getPoints());
+		} else {
+			for (int i = 0; i < kids.length; i++) {
+				kids[i].drawLeaves(g);
+			}
+		}
+	}
+
+	private void drawBounds(PGraphics g, PVector[] rect) {
+		g.rect(rect[0].x, rect[0].y, rect[1].x, rect[1].y);
+	}
+}
---
+++ tags/REL_0.4/RB-0.4/src/wordcram/WordCramEngine.java
@@ -0,0 +1,230 @@
+package wordcram;
+
+/*
+Copyright 2010 Daniel Bernier
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import java.awt.*;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+import processing.core.*;
+
+class WordCramEngine {
+
+	private PGraphics destination;
+	
+	private WordFonter fonter;
+	private WordSizer sizer;
+	private WordColorer colorer;
+	private WordAngler angler;
+	private WordPlacer placer;
+	private WordNudger nudger;
+	private WordShaper wordShaper;
+	
+	private Word[] words; // just a safe copy
+	private EngineWord[] eWords;
+	private int eWordIndex = -1;
+	
+	private RenderOptions renderOptions;
+	
+	private Timer timer = Timer.getInstance();
+	
+	WordCramEngine(PGraphics destination, Word[] words, WordFonter fonter, WordSizer sizer, WordColorer colorer, WordAngler angler, WordPlacer placer, WordNudger nudger, WordShaper shaper, BBTreeBuilder bbTreeBuilder, RenderOptions renderOptions) {
+		
+		if (destination.getClass().equals(PGraphics2D.class)) {
+			throw new Error("WordCram can't work with P2D buffers, sorry - try using JAVA2D.");
+		}
+		
+		this.destination = destination;
+		
+		this.fonter = fonter;
+		this.sizer = sizer;
+		this.colorer = colorer;
+		this.angler = angler;
+		this.placer = placer;
+		this.nudger = nudger;
+		
+		this.renderOptions = renderOptions;
+		
+		this.wordShaper = shaper;
+		
+		this.words = words;
+		timer.start("making shapes");
+		this.eWords = wordsIntoEngineWords(words, bbTreeBuilder);
+		timer.end("making shapes");
+	}
+	
+	private EngineWord[] wordsIntoEngineWords(Word[] words, BBTreeBuilder bbTreeBuilder) {
+		ArrayList<EngineWord> engineWords = new ArrayList<EngineWord>();
+		
+		int maxNumberOfWords = renderOptions.maxNumberOfWordsToDraw >= 0 ?
+								renderOptions.maxNumberOfWordsToDraw :
+								words.length;
+		for (int i = 0; i < maxNumberOfWords; i++) {
+			
+			Word word = words[i];
+			EngineWord eWord = new EngineWord(word, i, words.length, bbTreeBuilder);
+			
+			PFont wordFont = word.getFont(fonter);
+			float wordSize = word.getSize(sizer, i, words.length);
+			float wordAngle = word.getAngle(angler);
+			
+			timer.start("making a shape");
+			Shape shape = wordShaper.getShapeFor(eWord.word.word, wordFont, wordSize, wordAngle, renderOptions.minShapeSize);
+			timer.end("making a shape");
+			
+			if (shape == null) {
+				skipWord(word, WordCram.SHAPE_WAS_TOO_SMALL);
+			}
+			else {
+				eWord.setShape(shape);
+				engineWords.add(eWord);  // DON'T add eWords with no shape.
+			}
+		}
+		
+		for (int i = maxNumberOfWords; i < words.length; i++) {
+			skipWord(words[i], WordCram.WAS_OVER_MAX_NUMBER_OF_WORDS);
+		}
+		
+		return engineWords.toArray(new EngineWord[0]);
+	}
+	
+	private void skipWord(Word word, int reason) {
+		// TODO delete these properties when starting a sketch, in case it's a re-run w/ the same words.
+		// NOTE: keep these as properties, because they (will be) deleted when the WordCramEngine re-runs.
+		word.wasSkippedBecause(reason);
+	}
+	
+	boolean hasMore() {
+		return eWordIndex < eWords.length-1;
+	}
+	
+	void drawAll() {
+		timer.start("drawAll");
+		while(hasMore()) {
+			drawNext();
+		}
+		timer.end("drawAll");
+		//System.out.println(timer.report());
+	}
+	
+	void drawNext() {
+		if (!hasMore()) return;
+		
+		EngineWord eWord = eWords[++eWordIndex];
+		
+		timer.start("placeWord");
+		boolean wasPlaced = placeWord(eWord);
+		timer.end("placeWord");
+					
+		if (wasPlaced) { // TODO unit test (somehow)
+			timer.start("drawWordImage");
+			drawWordImage(eWord);
+			timer.end("drawWordImage");
+		}
+	}	
+	
+	private boolean placeWord(EngineWord eWord) {
+		Word word = eWord.word;
+		Rectangle2D rect = eWord.getShape().getBounds2D(); // TODO can we move these into EngineWord.setDesiredLocation? Does that make sense?		
+		int wordImageWidth = (int)rect.getWidth();
+		int wordImageHeight = (int)rect.getHeight();
+		
+		eWord.setDesiredLocation(placer, eWords.length, wordImageWidth, wordImageHeight, destination.width, destination.height);
+		
+		// Set maximum number of placement trials
+		int maxAttemptsToPlace = renderOptions.maxAttemptsToPlaceWord > 0 ?
+									renderOptions.maxAttemptsToPlaceWord :
+									calculateMaxAttemptsFromWordWeight(word);
+		
+		EngineWord lastCollidedWith = null;
+		for (int attempt = 0; attempt < maxAttemptsToPlace; attempt++) {
+			
+			eWord.nudge(nudger.nudgeFor(word, attempt));
+			
+			PVector loc = eWord.getCurrentLocation();
+			if (loc.x < 0 || loc.y < 0 || loc.x + wordImageWidth >= destination.width || loc.y + wordImageHeight >= destination.height) {
+				timer.count("OUT OF BOUNDS");
+				continue;
+			}
+			
+			if (lastCollidedWith != null && eWord.overlaps(lastCollidedWith)) {
+				timer.count("CACHE COLLISION");
+				continue;
+			}
+			
+			boolean foundOverlap = false;
+			for (int i = 0; !foundOverlap && i < eWordIndex; i++) {
+				EngineWord otherWord = eWords[i];
+				if (eWord.overlaps(otherWord)) {
+					foundOverlap = true;
+					lastCollidedWith = otherWord;
+				}
+			}
+			
+			if (!foundOverlap) {
+				timer.count("placed a word");
+				eWord.finalizeLocation();
+				return true;
+			}
+		}
+		
+		skipWord(eWord.word, WordCram.NO_SPACE);
+		timer.count("couldn't place a word");
+		return false;
+	}
+
+	private int calculateMaxAttemptsFromWordWeight(Word word) {
+		return (int)((1.0 - word.weight) * 600) + 100;
+	}
+	
+	private void drawWordImage(EngineWord word) {
+		GeneralPath path2d = new GeneralPath(word.getShape());
+		
+		Graphics2D g2 = (Graphics2D)destination.image.getGraphics();
+			
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2.setPaint(new Color(word.word.getColor(colorer), true));
+		g2.fill(path2d);
+	}
+	
+	Word getWordAt(float x, float y) {
+		for (int i = 0; i < eWords.length; i++) {
+			if (eWords[i].wasPlaced()) {
+				Shape shape = eWords[i].getShape();
+				if (shape.contains(x, y)) {
+					return eWords[i].word;
+				}
+			}
+		}
+		return null;
+	}
+
+	Word[] getSkippedWords() {
+		ArrayList<Word> skippedWords = new ArrayList<Word>();
+		for (int i = 0; i < words.length; i++) {
+			if (words[i].wasSkipped()) {
+				skippedWords.add(words[i]);
+			}
+		}
+		return skippedWords.toArray(new Word[0]);
+	}
+	
+	float getProgress() {
+		return (float)this.eWordIndex / this.eWords.length;
+	}
+}
