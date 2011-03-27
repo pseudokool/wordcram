d28f386e50c1e4f8b9671bfa8459256d7d73fb0e
danbernier <danbernier@db2b9112-8060-c212-668a-ea1c16feaf2a>
2011-03-27 13:06:55 +0000

    Added release 0.4.1

---
+++ tags/REL_0.4.1/src/wordcram/RenderOptions.java
@@ -0,0 +1,7 @@
+package wordcram;
+
+class RenderOptions {
+	int maxAttemptsToPlaceWord = -1; // default: based on Word weight
+	int maxNumberOfWordsToDraw = -1; // default: unlimited
+	int minShapeSize = 7;
+}
